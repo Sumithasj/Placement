56ee9d4adb2439231b7d2cd452b237d2a2dc4852
Sumithasj <50878636+Sumithasj@users.noreply.github.com>
2019-05-29 03:27:10 -0700

    Update Bubble.c

--- C/Bubble.c
+++ C/Bubble.c
@@ -1,1 +1,22 @@
 
+#include<stdio.h>
+void main()
+{
+    int n,number[20],temp,i,j;
+    printf("enter total number");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++)
+        scanf("%d",&number[i]);
+    for(i=1;i<=n;++i)
+        for(j=0;j<=i;++j)
+            if(number[j]>number[j+1])
+            {
+                temp=number[j];
+                number[j]=number[j+1];
+                number[j+1]=temp;
+            }
+    printf("sorting array");
+    for(i=0;i<n;++i)
+        printf("%d",number[i]);
+    return 0;
+}
